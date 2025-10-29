e35de03841dc60a1b7273a35d27663cd1acc0a67
tamnk74 <khac.tam.94@gmail.com>
2025-10-29 21:57:17 +0700

    feat: add mobile feature

---
+++ fullstack-dev/assets/js/138e0e15.20066491.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs_site=self.webpackChunkdocs_site||[]).push([[4921],{41597:s=>{s.exports=JSON.parse('{"name":"@easyops-cn/docusaurus-search-local","id":"default"}')}}]);
